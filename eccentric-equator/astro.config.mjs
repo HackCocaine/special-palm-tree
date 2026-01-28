95288d8eb6752b0f59e15784008f4748174aedf5
chooksuspect <60366182+HackCocaine@users.noreply.github.com>
2026-01-28 07:19:39 -0300

    Workflow

--- eccentric-equator/astro.config.mjs
+++ eccentric-equator/astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // site: 'https://hackfluency.com',
+  // Si usas GitHub Pages con un subdirectorio, descomenta y ajusta:
+  base: '/special-palm-tree',
+});
